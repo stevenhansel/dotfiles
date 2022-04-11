aceb1a7f5ea705ae46cf824de2d60f1de0044c64
Myron <stevenhanselgo@gmail.com>
2022-04-11 12:08:03 +0700

    feat: install packer and paper color colorscheme

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,78 +1,20 @@
-local set = vim.opt
-local map = vim.api.nvim_set_keymap
-local cmd = vim.cmd
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
--- hide intro message
-set.shortmess = 'I'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
--- backup configuration
-set.backup = false
-set.writebackup = false
-set.swapfile = false
-set.history = 750
+require('general/main')
 
--- tab configuration
-set.expandtab = true
-set.smarttab = true
+return require('packer').startup(function(use)
+  -- Packer, the package manager :D
+  use 'wbthomason/packer.nvim'
 
-set.tabstop = 2
-set.shiftwidth = 2
-set.softtabstop = 2
+  -- Colorscheme
+  use 'NLKNguyen/papercolor-theme'
 
-set.ai = true
-set.si = true
-set.wrap = true
-
--- line numbering
-set.number = true
-set.relativenumber = true
-
--- general mapping
-set.clipboard = 'unnamedplus'
-set.ignorecase = true
-
--- syntax highlighting
-cmd[[ syntax on ]]
-set.encoding = 'utf8'
-set.ffs = 'unix,dos,mac'
-
--- colorscheme
-set.termguicolors = true
-set.background = 'dark'
-cmd[[ set t_Co=256 ]]
-cmd[[]]
-
--- cursor style
-cmd[[ highlight Visual cterm=NONE ctermbg=236 ctermfg=NONE guibg=Grey40 ]]
-cmd[[ highlight LineNr cterm=none ctermfg=240 guifg=#2b506e guibg=#000000 ]]
-
--- filetype plugins
-cmd[[ filetype plugin on ]]
-cmd[[ filetype indent on ]]
-
--- tab mappings
-map('n', 'tn', ':tabnew<cr>', {noremap = true})
-map('n', 'tc', ':tabclose<cr>', {noremap = true})
-map('n', '<S-Tab>', ':tabprev<Return>', {noremap=true})
-map('n', '<Tab>', ':tabnext<Return>', {noremap=true})
-
--- window mappings
-map('n', 'ss', ':split<Return><C-w>w', {noremap=true})
-map('n', 'sv', ':vsplit<Return><C-w>w', {noremap=true})
-
-map('n', '<Space>', '<C-w>w', {noremap=true})
-
-map('n', 's<left>', '<C-w>h', {noremap=true})
-map('n', 's<up>', '<C-w>k', {noremap=true})
-map('n', 's<down>', '<C-w>j', {noremap=true})
-map('n', 's<right>', '<C-w>l', {noremap=true})
-
-map('n', 'sh', '<C-w>h', {noremap=true})
-map('n', 'sk', '<C-w>k', {noremap=true})
-map('n', 'sj', '<C-w>j', {noremap=true})
-map('n', 'sl', '<C-w>l', {noremap=true})
-
-map('n', 'csh', '<C-w><', {noremap=true})
-map('n', 'csk', '<C-w>+', {noremap=true})
-map('n', 'csj', '<C-w>-', {noremap=true})
-map('n', 'csl', '<C-w>>', {noremap=true})
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
---
+++ vim/.config/nvim/lua/general/main.lua
@@ -0,0 +1,79 @@
+local set = vim.opt
+local map = vim.api.nvim_set_keymap
+local cmd = vim.cmd
+
+-- hide intro message
+set.shortmess = 'I'
+
+-- backup configuration
+set.backup = false
+set.writebackup = false
+set.swapfile = false
+set.history = 750
+
+-- tab configuration
+set.expandtab = true
+set.smarttab = true
+
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
+
+set.ai = true
+set.si = true
+set.wrap = true
+
+-- line numbering
+set.number = true
+set.relativenumber = true
+set.laststatus = 2
+
+-- general mapping
+set.clipboard = 'unnamedplus'
+set.ignorecase = true
+
+-- syntax highlighting
+cmd[[ syntax on ]]
+set.encoding = 'utf8'
+set.ffs = 'unix,dos,mac'
+
+-- colorscheme
+set.termguicolors = true
+set.background = 'dark'
+cmd[[ set t_Co=256 ]]
+cmd[[ colorscheme PaperColor ]]
+
+-- cursor style
+cmd[[ highlight Visual cterm=NONE ctermbg=236 ctermfg=NONE guibg=Grey40 ]]
+cmd[[ highlight LineNr cterm=none ctermfg=240 guifg=#2b506e guibg=#000000 ]]
+
+-- filetype plugins
+cmd[[ filetype plugin on ]]
+cmd[[ filetype indent on ]]
+
+-- tab mappings
+map('n', 'tn', ':tabnew<cr>', {noremap = true})
+map('n', 'tc', ':tabclose<cr>', {noremap = true})
+map('n', '<S-Tab>', ':tabprev<Return>', {noremap=true})
+map('n', '<Tab>', ':tabnext<Return>', {noremap=true})
+
+-- window mappings
+map('n', 'ss', ':split<Return><C-w>w', {noremap=true})
+map('n', 'sv', ':vsplit<Return><C-w>w', {noremap=true})
+
+map('n', '<Space>', '<C-w>w', {noremap=true})
+
+map('n', 's<left>', '<C-w>h', {noremap=true})
+map('n', 's<up>', '<C-w>k', {noremap=true})
+map('n', 's<down>', '<C-w>j', {noremap=true})
+map('n', 's<right>', '<C-w>l', {noremap=true})
+
+map('n', 'sh', '<C-w>h', {noremap=true})
+map('n', 'sk', '<C-w>k', {noremap=true})
+map('n', 'sj', '<C-w>j', {noremap=true})
+map('n', 'sl', '<C-w>l', {noremap=true})
+
+map('n', 'csh', '<C-w><', {noremap=true})
+map('n', 'csk', '<C-w>+', {noremap=true})
+map('n', 'csj', '<C-w>-', {noremap=true})
+map('n', 'csl', '<C-w>>', {noremap=true})
